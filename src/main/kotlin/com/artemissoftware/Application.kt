997e7ca5ee00fbef44e60c85fd2d913c8fb2ea87
gustavo <gustavo.isaac@gmail.com>
2024-01-08 05:25:46 +0000

    mongo client

--- src/main/kotlin/com/artemissoftware/Application.kt
+++ src/main/kotlin/com/artemissoftware/Application.kt
@@ -2,12 +2,22 @@ package com.artemissoftware
 
 import com.artemissoftware.plugins.*
 import io.ktor.server.application.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    val mongoPw = System.getenv("MONGO_PW")
+    val dbName = "ktor-auth"
+    val db = KMongo.createClient(
+        connectionString = "mongodb+srv://philipplackner:$mongoPw@cluster0.n2mogbs.mongodb.net/$dbName?retryWrites=true&w=majority"
+    ).coroutine
+        .getDatabase(dbName)
+
+
     configureSerialization()
     configureMonitoring()
     configureSecurity()
