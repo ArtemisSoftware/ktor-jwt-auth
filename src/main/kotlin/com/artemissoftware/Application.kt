c8fe1abdd8f3351cedc3fc3840d609bd60fc55a3
gustavo <gustavo.isaac@gmail.com>
2024-01-09 05:42:15 +0000

    TokenConfig

--- src/main/kotlin/com/artemissoftware/Application.kt
+++ src/main/kotlin/com/artemissoftware/Application.kt
@@ -1,7 +1,11 @@
 package com.artemissoftware
 
+import com.artemissoftware.data.models.user.MongoUserDataSource
+import com.artemissoftware.data.models.user.User
 import com.artemissoftware.plugins.*
 import io.ktor.server.application.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
@@ -13,10 +17,11 @@ fun Application.module() {
     val mongoPw = System.getenv("MONGO_PW")
     val dbName = "ktor-auth"
     val db = KMongo.createClient(
-        connectionString = "mongodb+srv://philipplackner:$mongoPw@cluster0.n2mogbs.mongodb.net/$dbName?retryWrites=true&w=majority"
+        connectionString = "mongodb+srv://artemis:$mongoPw@cluster0.n2mogbs.mongodb.net/$dbName?retryWrites=true&w=majority"
     ).coroutine
         .getDatabase(dbName)
 
+    val userDataSource = MongoUserDataSource(db)
 
     configureSerialization()
     configureMonitoring()
