4ac713a7d4964f9c73c13e91809b61bbeadcee8a
gustavo <gustavo.isaac@gmail.com>
2024-01-10 05:58:43 +0000

    AuthRoutes

---
+++ src/main/kotlin/com/artemissoftware/routes/auth/AuthRoutes.kt
@@ -0,0 +1,127 @@
+package com.artemissoftware.routes.auth
+
+import com.artemissoftware.data.models.user.User
+import com.artemissoftware.data.models.user.UserDataSource
+import com.artemissoftware.data.requests.AuthRequest
+import com.artemissoftware.data.responses.AuthResponse
+import com.artemissoftware.routes.auth.authenticate
+import com.artemissoftware.security.hashing.HashingService
+import com.artemissoftware.security.hashing.SaltedHash
+import com.artemissoftware.security.token.TokenClaim
+import com.artemissoftware.security.token.TokenConfig
+import com.artemissoftware.security.token.TokenService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.apache.commons.codec.digest.DigestUtils
+
+fun Route.signUp(
+    hashingService: HashingService,
+    userDataSource: UserDataSource
+) {
+    post("signup") {
+
+//        val request = kotlin.runCatching {
+//            call.receiveNullable<AuthRequest>()
+//        }.getOrNull() ?: kotlin.run {
+//            call.respond(HttpStatusCode.BadRequest)
+//            return@post
+//        }
+
+        val request = call.receiveOrNull<AuthRequest>() ?: kotlin.run {
+            call.respond(HttpStatusCode.BadRequest)
+            return@post
+        }
+
+        val areFieldsBlank = request.username.isBlank() || request.password.isBlank()
+        val isPwTooShort = request.password.length < 8
+        if(areFieldsBlank || isPwTooShort) {
+            call.respond(HttpStatusCode.Conflict)
+            return@post
+        }
+
+        val saltedHash = hashingService.generateSaltedHash(request.password)
+        val user = User(
+            username = request.username,
+            password = saltedHash.hash,
+            salt = saltedHash.salt
+        )
+        val wasAcknowledged = userDataSource.insertUser(user)
+        if(!wasAcknowledged)  {
+            call.respond(HttpStatusCode.Conflict)
+            return@post
+        }
+
+        call.respond(HttpStatusCode.OK)
+    }
+}
+
+fun Route.signIn(
+    userDataSource: UserDataSource,
+    hashingService: HashingService,
+    tokenService: TokenService,
+    tokenConfig: TokenConfig
+) {
+    post("signin") {
+        val request = call.receiveOrNull<AuthRequest>() ?: kotlin.run {
+            call.respond(HttpStatusCode.BadRequest)
+            return@post
+        }
+
+        val user = userDataSource.getUserByUsername(request.username)
+        if(user == null) {
+            call.respond(HttpStatusCode.Conflict, "Incorrect username or password")
+            return@post
+        }
+
+        val isValidPassword = hashingService.verify(
+            value = request.password,
+            saltedHash = SaltedHash(
+                hash = user.password,
+                salt = user.salt
+            )
+        )
+        if(!isValidPassword) {
+            println("Entered hash: ${DigestUtils.sha256Hex("${user.salt}${request.password}")}, Hashed PW: ${user.password}")
+            call.respond(HttpStatusCode.Conflict, "Incorrect username or password")
+            return@post
+        }
+
+        val token = tokenService.generate(
+            config = tokenConfig,
+            TokenClaim(
+                name = "userId",
+                value = user.id.toString()
+            )
+        )
+
+        call.respond(
+            status = HttpStatusCode.OK,
+            message = AuthResponse(
+                token = token
+            )
+        )
+    }
+}
+
+fun Route.authenticate() {
+    authenticate {
+        get("authenticate") {
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+
+fun Route.getSecretInfo() {
+    authenticate {
+        get("secret") {
+            val principal = call.principal<JWTPrincipal>()
+            val userId = principal?.getClaim("userId", String::class)
+            call.respond(HttpStatusCode.OK, "Your userId is $userId")
+        }
+    }
+}
