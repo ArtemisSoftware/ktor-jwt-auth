45d73cb8c4f521ee2a7f870c68ea19964f8b5b37
gustavo <gustavo.isaac@gmail.com>
2024-01-10 05:39:48 +0000

    jwt config

--- src/main/kotlin/com/artemissoftware/Application.kt
+++ src/main/kotlin/com/artemissoftware/Application.kt
@@ -1,11 +1,11 @@
 package com.artemissoftware
 
 import com.artemissoftware.data.models.user.MongoUserDataSource
-import com.artemissoftware.data.models.user.User
 import com.artemissoftware.plugins.*
+import com.artemissoftware.security.hashing.SHA256HashingService
+import com.artemissoftware.security.token.JwtTokenService
+import com.artemissoftware.security.token.TokenConfig
 import io.ktor.server.application.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
@@ -23,8 +23,17 @@ fun Application.module() {
 
     val userDataSource = MongoUserDataSource(db)
 
+    val tokenService = JwtTokenService()
+    val tokenConfig = TokenConfig(
+        issuer = environment.config.property("jwt.issuer").getString(),
+        audience = environment.config.property("jwt.audience").getString(),
+        expiresIn = 365L * 1000L * 60L * 60L * 24L,
+        secret = System.getenv("JWT_SECRET")
+    )
+    val hashingService = SHA256HashingService()
+
     configureSerialization()
     configureMonitoring()
-    configureSecurity()
+    configureSecurity(tokenConfig = tokenConfig)
     configureRouting()
 }
